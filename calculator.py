842e54adee12b48287e664e3dd58971f09461da1
HaiAnh-25 <haianhchuc06@gmail.com>
2026-06-06 19:27:10 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 import sys
-import calcobj.py
+import calcobj
 s = sys.stdin.readlines()
+s = s[0].strip()
+l = []
 if '+' in s:
     l = s.split('+')
 elif '-' in s:
@@ -9,3 +11,14 @@ elif '*' in s:
     l = s.split('*')
 elif '/' in s:
     l = s.split('/')
+m = int(l[0])
+n = int(l[1])
+
+if '+' in s:
+    print(m + n)
+elif '-' in s:
+    print(m - n)
+elif '*' in s:
+    print(m * n)
+elif '/' in s:
+    print(m / n)
